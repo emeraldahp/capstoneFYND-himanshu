9218d5cd78029b83436399ccef9da1dbc850d6cc
Himanshu Patel <himanshu.patel.main@gmail.com>
2023-01-18 09:46:49 +0530

    dotenv, userschema

--- courseApp-server/src/index.js
+++ courseApp-server/src/index.js
@@ -1,9 +1,12 @@
+const dotenv = require('dotenv')
 const express = require('express');
 const { connectDB } = require('./db/connect');
 const coursesApiRouter = require('./routes/courses.routes')
 const cors = require('cors')
 
 const app = express();
+dotenv.config()
+
 app.use(cors({
     origin: "*"
 }))
@@ -15,5 +18,5 @@ app.use(express.urlencoded({extended:false}))
 
 app.use('/courses', coursesApiRouter)
 
-connectDB("mongodb://localhost:27017/courseappdb")
+connectDB(process.env.MONGO_URI)
 app.listen(8531)

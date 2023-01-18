5daa2a00c23cc0e0aed17a984d07e1ec1f9343c8
Himanshu Patel <himanshu.patel.main@gmail.com>
2023-01-18 10:44:05 +0530

    basic login logic test

--- courseApp-server/src/controllers/users.controllers.js
+++ courseApp-server/src/controllers/users.controllers.js
@@ -1,7 +1,18 @@
 const User = require('../models/User')
 
 const userLogin = async (req, res, next) => {
-    console.log("userlogin")
+    const credentials = req.body
+    const {userName, password} = credentials
+    const check = await User.findOne({userName})
+    if(check === null){
+        console.log("No user found")
+    }
+    else{
+        if(check.password==password)
+            console.log("user:", userName, "loginSuccess")
+        else
+            console.log("wrong password")
+    }
 }
 
 module.exports = {

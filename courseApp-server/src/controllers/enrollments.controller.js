2ff5ce148ebe1a1e70618e48eacbce04e43b1120
Himanshu Patel <himanshu.patel.main@gmail.com>
2023-01-22 17:27:34 +0530

    get enrollments

--- courseApp-server/src/controllers/enrollments.controller.js
+++ courseApp-server/src/controllers/enrollments.controller.js
@@ -2,6 +2,9 @@ const Enrollment = require('../models/Enrollment')
 
 const getEnrollments = async (req, res, next) => {
     console.log(req.query.userName)
+    const userName = req.query.userName
+    enrollments = await Enrollment.find({userName})
+    console.log(enrollments)
 }
 
 module.exports = {

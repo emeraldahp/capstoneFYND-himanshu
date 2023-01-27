e8f86ff05279049ba1de06555fcccbad198b40b0
Himanshu Patel <himanshu.patel.main@gmail.com>
2023-01-27 16:59:23 +0530

    login and register control

---
+++ courseApp/src/components/RegisterComp.vue
@@ -0,0 +1,43 @@
+<script>
+
+export default {
+    name: "registercomp",
+    props: {
+        registerType:{}
+    },
+    data() {
+        return {
+            registerData: {
+                name: "",
+                email: "",
+                password: ""
+            }
+        }
+    },
+    methods: {
+        async userRegister() {
+            console.log("userRegister", this.registerData)
+        },
+        async tutorRegister() {
+            console.log("tutorRegister", this.registerData)
+        },
+        async adminRegister() {
+            console.log("adminRegister", this.registerData)
+        }
+
+    }
+}
+</script>
+<template>
+    <div>
+        Register Form
+        <form @submit.prevent="">
+        Enter {{registerType}}name: <input type="text" v-model="this.registerData.name"> <br>
+        Enter email: <input type="text" v-model="this.registerData.email"> <br>
+        Enter password: <input type="text" v-model="this.registerData.password"> <br>
+        <button v-if="registerType=='user'" @click="userRegister">UserRegister</button>  
+        <button v-if="registerType=='tutor'" @click="tutorRegister">TutorRegister</button>  
+        <button v-if="registerType=='admin'" @click="adminRegister">AdminRegister</button>      
+    </form> 
+    </div>
+</template>
--- courseApp/src/views/LoginView.vue
+++ courseApp/src/views/LoginView.vue
@@ -1,10 +1,21 @@
 <script>
-import UserLogin from '../components/UserLogin.vue'
+import LoginComp from '../components/LoginComp.vue'
+import RegisterComp from '../components/RegisterComp.vue'
+
 
 export default {
     name: 'loginview',
     components: {
-        UserLogin
+        LoginComp,
+        RegisterComp
+    },
+    data() {
+        return {
+            control: {
+                type: 'login',
+                for: 'user'
+            }
+        }
     }
 }
 
@@ -12,7 +23,28 @@ export default {
 
 <template>
     <div>
+        {{this.control}}
         LoginView <br>
-        <user-login/>
+        <div v-if="this.control.type=='login'">
+            {{ this.control.for }}Login <br>
+            <login-comp :loginType="this.control.for"></login-comp>
+            <button @click="this.control.type='register'">Register {{ this.control.for }}</button>
+        </div>
+        <div v-if="this.control.type=='register'">
+            {{ this.control.for }}Register <br>
+            <register-comp :registerType="this.control.for"></register-comp>
+            <button @click="this.control.type='login'">Login {{ this.control.for }}</button>
+        </div>
+
+        <br>
+        <div v-if="this.control.for == 'user' || this.control.for == 'admin'">
+            <button @click="this.control.for = 'tutor' ">Login as Tutor</button>
+        </div>
+        <div v-if="this.control.for == 'user' || this.control.for == 'tutor'">
+            <button @click="this.control.for = 'admin'">Login as Admin</button>
+        </div>
+        <div v-if="this.control.for == 'tutor' || this.control.for == 'admin'">
+            <button @click="this.control.for = 'user'">Login as User</button>
+        </div>
     </div>
 </template>

2f0f6499680ccc987b8ccf9ed733ea4288452610
Himanshu Patel <himanshu.patel.main@gmail.com>
2023-01-31 17:08:28 +0530

    myprofile and my questions

--- courseApp/src/components/MyProfile.vue
+++ courseApp/src/components/MyProfile.vue
@@ -1,8 +1,39 @@
 <script>
+import axios from 'axios';
+
+export default {
+    name: "myprofile",
+    data() {
+        return {
+            enrollments:[],
+            completedCourses:[],
+            incompleteCourses:[],
+        }
+    },
+    mounted() {
+        axios.get(import.meta.env.VITE_API_URL + '/enrollments', {params:{userName: this.$store.state.userData.userName}}).then(res => {
+            this.enrollments = res.data.data
+            this.enrollments.forEach(element => {
+                if(element.isCourseComplete == true) {
+                    this.completedCourses.push(element)
+                }
+                else
+                    this.incompleteCourses.push(element)
+            })
+        })
+    }
+}
 
 </script>
 <template>
     MyProfile <br>
-    Completed Cources <br>
-    Courses in Progress <br>
+    <h4>Completed Courses</h4>
+    <div v-for="course in completedCourses" :key="course._id" >
+        {{course.courseName}}
+    </div>
+    <h4>Courses in Progress</h4>
+    <div v-for="course in incompleteCourses" :key="course._id" >
+        {{course.courseName}}
+    </div>
+
 </template>
